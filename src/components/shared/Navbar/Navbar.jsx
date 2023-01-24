9c553b0ef00126e0bd2c08014280ed45b86160e0
magobasti <abastidas94@gmail.com>
2023-01-23 19:25:43 -0500

    Conexión de rutas para navegación

---
+++ src/components/shared/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div>Navbar</div>
+        <div>
+            <h1>Home Page</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
