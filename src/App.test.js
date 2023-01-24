3ec8058b5322b9a790c2840ad30b4e077b61188f
magobasti <abastidas94@gmail.com>
2023-01-24 18:15:16 -0500

    Carga para estadisticas

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,23 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store';
 import App from './App';
+import Navbar from './components/shared/Navbar/Navbar';
+import ReactDOM from 'react-dom';
+
 
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
+            <BrowserRouter>
       <App />
+      </BrowserRouter>
     </Provider>
   );
-
+  
   expect(getByText(/learn/i)).toBeInTheDocument();
 });
+
