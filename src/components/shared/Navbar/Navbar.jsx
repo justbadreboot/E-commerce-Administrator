9ff73c3b82c0a65cbf756892d4387566e2b9d2f4
magobasti <abastidas94@gmail.com>
2023-01-23 23:14:01 -0500

    Implementación de iconos para navbar

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -1,5 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { BsGraphUp } from "react-icons/bs";
+import { AiFillMedicineBox } from "react-icons/ai";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { MdStoreMallDirectory, MdProductionQuantityLimits, MdOutlineMedicalServices, MdLocalOffer} from "react-icons/md";
 
 const Navbar = () => {
   return (
@@ -7,37 +12,61 @@ const Navbar = () => {
         <ul className="flex flex-col pl-0 mb-0">
             <div className='flex'>
             <NavLink to="/" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
-            <i class="fa-solid fa-house-user"></i>
-                <p>Home</p>
+                <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+                <BsGraphUp/> 
+                </div>
+                     <p>Home</p>
             </NavLink>
             </div>
             <div>
             <NavLink to="/store" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
+            <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+            <MdStoreMallDirectory/> 
+            </div>
                 <p>Store</p>
             </NavLink>
             </div>
             <div>
             <NavLink to="/products" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
+            <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+                <MdProductionQuantityLimits/>
+                </div>
                 <p>Products</p>
             </NavLink>
             </div>
             <div>
             <NavLink to="/services" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
+            <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+
+                <MdOutlineMedicalServices/>
+                </div>
                 <p>Services</p>
             </NavLink>
             </div>
             <div>
             <NavLink to="/doctors" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
+            <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+
+                <AiFillMedicineBox/>
+                </div>
                 <p>Doctors</p>
             </NavLink>
             </div>
             <div>
             <NavLink to="/promotions" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
+            <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+
+                <MdLocalOffer/>
+                </div>
                 <p>Promotions</p>
             </NavLink>
             </div>
             <div>
             <NavLink to="/orders" className="py-2.7 shadow-soft-xl text-sm ease-nav-brand my-0 mx-4 flex items-center whitespace-nowrap rounded-lg bg-white px-4 font-semibold text-slate-700 transition-colors">
+            <div className='shadow-soft-2xl mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-white bg-center stroke-0 text-center xl:p-2.5'>
+
+                <FaMoneyBillAlt/>
+                </div>
                 <p>Orders</p>
             </NavLink>
             </div>
