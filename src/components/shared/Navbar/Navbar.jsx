4ef5e8c2cb2defc8c8b171c1413e8d90ca902545
magobasti <abastidas94@gmail.com>
2023-01-28 00:10:30 -0500

    Mejora de responsibidad

--- src/components/shared/Navbar/Navbar.jsx
+++ src/components/shared/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ import { MdStoreMallDirectory, MdProductionQuantityLimits, MdOutlineMedicalServi
 const Navbar = () => {
     const [selectedTab, setSelectedTab] = useState('home');
     return (
-        <div className='max-w-20 ease-nav-brand z-990 block inset-y-0 my-4  w-52 -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent'>
+        <div className='max-w-20 ease-nav-brand z-990 block inset-y-0 my-4  w-0 lg:w-52  -translate-x-full flex-wrap items-center justify-between overflow-y-auto rounded-2xl border-0 bg-white p-0 antialiased shadow-none transition-transform duration-200 xl:left-0 xl:translate-x-0 xl:bg-transparent'>
             <div>
                 <img src="./images/logo-template.png" className='inline w-40 transition-all duration-200 h-full mx-5 '/>
             </div>
