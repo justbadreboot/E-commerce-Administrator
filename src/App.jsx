5deeceb3f38820cbd97e1898abaf59d8557b48f1
magobasti <abastidas94@gmail.com>
2023-01-29 18:12:09 -0500

    Creación base de Login y enrutamiento

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,26 @@
 import React, { useState } from 'react';
 
-import './App.css';
 import Login from './components/rutas/Login';
-
 import RutasAdministrador from './components/rutas/RutasAdministrador';
 import RutasReaprtidor from './components/rutas/RutasReaprtidor';
 
-
 function App() {
-  const [isLoggin,setIsLoggin]=useState(<Login/>)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const rol = localStorage.getItem('rol');
 
-  const rol=localStorage.getItem('rol')
-  if(rol==="Admin"){
-    setIsLoggin(<RutasAdministrador/>)
-  }
-  else if(rol==="Repartidor"){
-    setIsLoggin(<RutasReaprtidor/>)
-  }
   return (
     <>
-    {isLoggin}
+      {rol=="Admin" ||rol=="Repartidor" ? (
+        <>
+          {localStorage.getItem('rol') === "Admin" ? (
+            <RutasAdministrador />
+          ) : (
+            <RutasReaprtidor />
+          )}
+        </>
+      ) : (
+        <Login/>
+      )}
     </>
   );
 }
--- src/components/rutas/Login.jsx
+++ src/components/rutas/Login.jsx
@@ -1,10 +1,98 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 const Login = () => {
-  return (
-    <div>Cambia estado
-    </div>
-  )
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+
+    const [errorEmail, setErrorEmail] = useState(false);
+    const [errorPassword, setErrorPassword] = useState(false);
+
+    const usuario1 = {
+        email: 'usuario1@correo.com',
+        password: 'contraseña1'
+    };
+
+    const usuario2 = {
+        email: 'usuario2@correo.com',
+        password: 'contraseña2'
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        // Resetear estado de errores
+        setErrorEmail(false);
+        setErrorPassword(false);
+        if (!email) {
+            setErrorEmail(true);
+        }
+        if (!password) {
+            setErrorPassword(true);
+        }
+        // Si hay algún error, terminamos la función
+        if (errorEmail || errorPassword) {
+            return;
+        }
+
+        if (email === usuario1.email && password === usuario1.password) {
+            localStorage.setItem('rol', 'Admin')
+            // Recargar la página
+            window.location.reload();
+        } else if (email === usuario2.email && password === usuario2.password) {
+            setMessage('Las credenciales pertenecen a usuario2');
+        } else {
+            setMessage('Correo o contraseña incorrectos');
+        }
+
+    };
+
+    return (
+        <div className="bg-gray-100 flex justify-center  h-screen">
+            <div class="h-2 bg-primary"></div>
+
+            <div class="container mx-auto p-8">
+                <div class="mx-auto max-w-sm">
+                    <div class="w-3/5 text-center mx-auto pb-3">
+                        <img src='./images/logo-template.png' className='p-0 mx-auto my-auto' />
+                    </div>
+
+
+                    <div class="bg-white rounded shadow drop-shadow-xl">
+                        <div class="border-b py-8 font-bold text-black text-center text-xl tracking-widest uppercase">
+                            Bienvenido!
+                        </div>
+                        <form class="bg-gray-50 px-10 py-10" onSubmit={handleSubmit}>
+
+
+                            <div class="mb-3 drop-shadow-lg">
+                                <input class="border w-full p-3" name="email" type="text" placeholder="E-Mail" value={email}
+                                    onChange={(event) => setEmail(event.target.value)} />
+                            </div>
+                            {errorEmail && <span className="text-red-500">El campo correo es requerido</span>}
+                            <div class="mb-6 drop-shadow-lg">
+                                <input class="border w-full p-3" name="password" type="password" placeholder="**************" value={password}
+                                    onChange={(event) => setPassword(event.target.value)} />
+                            </div>
+                            {errorPassword && <span className="text-red-500">El campo contraseña es requerido</span>}
+                            <div class="flex">
+                                <button class="bg-primary bg-green-100 w-full p-4 text-sm text-white uppercase font-bold tracking-wider">
+                                    Login
+                                </button>
+                            </div>
+                            {message && <p className="text-red-500">{message}</p>}
+
+                        </form>
+
+                        <div class="border-t px-10 py-6">
+                            <div class="flex justify-between">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Login
