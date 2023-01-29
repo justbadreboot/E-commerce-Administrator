01515934a0379941013a3cfad127370f87603b0d
magobasti <abastidas94@gmail.com>
2023-01-29 15:53:26 -0500

    Arreglo de rutas para redireccionamiento

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,22 @@ import './App.css';
 import Login from './components/rutas/Login';
 
 import RutasAdministrador from './components/rutas/RutasAdministrador';
+import RutasReaprtidor from './components/rutas/RutasReaprtidor';
 
 
 function App() {
   const [isLoggin,setIsLoggin]=useState(<Login/>)
 
   const rol=localStorage.getItem('rol')
-  if(rol=="Admin"){
-    
+  if(rol==="Admin"){
+    setIsLoggin(<RutasAdministrador/>)
+  }
+  else if(rol==="Repartidor"){
+    setIsLoggin(<RutasReaprtidor/>)
   }
   return (
     <>
     {isLoggin}
-
     </>
   );
 }
---
+++ src/components/rutas/RutasReaprtidor.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RutasReaprtidor = () => {
+  return (
+    <div>RutasReaprtidor</div>
+  )
+}
+
+export default RutasReaprtidor
