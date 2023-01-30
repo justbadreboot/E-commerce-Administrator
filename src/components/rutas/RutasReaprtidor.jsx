4543e3e53067057626cb97bf84609bb1983aec61
magobasti <abastidas94@gmail.com>
2023-01-29 19:10:22 -0500

    Conexion de repartidor

--- src/components/rutas/RutasReaprtidor.jsx
+++ src/components/rutas/RutasReaprtidor.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomeRepartidor from '../Repartidor/HomeRepartidor'
 
 const RutasReaprtidor = () => {
   return (
-    <div>RutasReaprtidor</div>
+    <div className="m-0 flex font-sans antialiased font-normal text-base leading-default  text-slate-500 ">
+        <Routes>
+            <Route path="/" element={<HomeRepartidor/>}/>
+        </Routes>
+        </div>
   )
 }
 
