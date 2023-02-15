a9c8e7a28be41c24fe6779e2c1fa7ea169f65b63
magobasti <abastidas94@gmail.com>
2023-02-15 01:06:29 -0500

    Arreglo

--- src/components/screens/Services.jsx
+++ src/components/screens/Services.jsx
@@ -11,14 +11,14 @@ import Loader from '../../Loader'
 const Services = () => {
   const dispatch = useDispatch();
   const [isLoading, setIsLoading] = useState(false)
-  const products1=useSelector(state=>state.service.data)
-
   useEffect(() => {
     dispatch(ServiceData());
   }, [dispatch]);
   useEffect(() => {
     dispatch(EspecialidadData());
   }, [dispatch]);
+  const products1=useSelector(state=>state.service.data)
+
   const especialidad=useSelector(state=>state.especialidad.data)
 
   console.log(products1)
@@ -198,7 +198,7 @@ const Services = () => {
                     <input
                       className='pl-8 text-sm focus:shadow-soft-primary-outline ease-soft leading-5.6 relative -ml-px block min-w-0 flex-auto rounded-lg border border-solid border-gray-300 bg-white py-2 pr-3 text-gray-700 transition-all placeholder:text-gray-500 focus:border-green-100 focus:outline-none focus:transition-shadow'
                       type="text"
-                      placeholder="Nombre o Doctor"
+                      placeholder="Nombre"
                       value={searchValue}
                       onChange={handleSearch}
                     />
